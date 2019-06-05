dd9ecb0266d2452e51ceb3bebeddbff0de6829e4
Angelo G. Del Regno <kholk11@gmail.com>
2019-06-05 11:07:03 +0200

    clk: qcom: alpha-pll: Replace driver with the one from Linux 5.2-rc3
    This is an import of the clk-alpha-pll driver from Linux 5.2-rc3.
    It replaces the downstream qcom driver, which is based on a very
    old version of this, but with additions for new qcom socs.
    
    The additions for SDM845 and SM8150 will be done later on this
    new driver base.
    
    Change-Id: I51a5ac90e9f89d8354d9644bddd794c8b1849c4b

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -7,16 +7,12 @@
 #include <linux/clk-provider.h>
 #include "clk-regmap.h"
 
-#define ALPHA_POST_DIV_EVEN_SHIFT	8
-#define ALPHA_POST_DIV_ODD_SHIFT	12
-
 /* Alpha PLL types */
 enum {
 	CLK_ALPHA_PLL_TYPE_DEFAULT,
 	CLK_ALPHA_PLL_TYPE_HUAYRA,
 	CLK_ALPHA_PLL_TYPE_BRAMMO,
 	CLK_ALPHA_PLL_TYPE_FABIA,
-	CLK_ALPHA_PLL_TYPE_TRION,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -26,16 +22,13 @@ enum {
 	PLL_OFF_ALPHA_VAL_U,
 	PLL_OFF_USER_CTL,
 	PLL_OFF_USER_CTL_U,
-	PLL_OFF_USER_CTL_U1,
 	PLL_OFF_CONFIG_CTL,
 	PLL_OFF_CONFIG_CTL_U,
-	PLL_OFF_CONFIG_CTL_U1,
 	PLL_OFF_TEST_CTL,
 	PLL_OFF_TEST_CTL_U,
 	PLL_OFF_STATUS,
 	PLL_OFF_OPMODE,
 	PLL_OFF_FRAC,
-	PLL_OFF_CAL_L_VAL,
 	PLL_OFF_MAX_REGS
 };
 
@@ -45,11 +38,6 @@ struct pll_vco {
 	unsigned long min_freq;
 	unsigned long max_freq;
 	u32 val;
-};
-
-struct pll_vco_data {
-	unsigned long freq;
-	u8 post_div_val;
 };
 
 /**
@@ -62,28 +50,12 @@ struct pll_vco_data {
 struct clk_alpha_pll {
 	u32 offset;
 	const u8 *regs;
-	bool inited;
-
-	struct alpha_pll_config *config;
-
-	u32 *soft_vote;
-	u32 soft_vote_mask;
-/* Soft voting values */
-#define PLL_SOFT_VOTE_PRIMARY	BIT(0)
-#define PLL_SOFT_VOTE_CPU	BIT(1)
-#define PLL_SOFT_VOTE_AUX	BIT(2)
 
 	const struct pll_vco *vco_table;
 	size_t num_vco;
-
-	const struct pll_vco_data *vco_data;
-	size_t num_vco_data;
-
 #define SUPPORTS_OFFLINE_REQ	BIT(0)
 #define SUPPORTS_FSM_MODE	BIT(2)
 #define SUPPORTS_DYNAMIC_UPDATE	BIT(3)
-	/* associated with soft_vote for multiple PLL software instances */
-#define SUPPORTS_FSM_VOTE	BIT(4)
 	u8 flags;
 
 	struct clk_regmap clkr;
@@ -117,14 +89,12 @@ struct alpha_pll_config {
 	u32 alpha_hi;
 	u32 config_ctl_val;
 	u32 config_ctl_hi_val;
-	u32 config_ctl_hi1_val;
 	u32 main_output_mask;
 	u32 aux_output_mask;
 	u32 aux2_output_mask;
 	u32 early_output_mask;
 	u32 alpha_en_mask;
 	u32 alpha_mode_mask;
-	u32 frac;
 	u32 pre_div_val;
 	u32 pre_div_mask;
 	u32 post_div_val;
@@ -143,30 +113,6 @@ extern const struct clk_ops clk_alpha_pll_postdiv_ro_ops;
 extern const struct clk_ops clk_alpha_pll_fabia_ops;
 extern const struct clk_ops clk_alpha_pll_fixed_fabia_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_fabia_ops;
-
-static const struct clk_div_table clk_alpha_div_table[] = {
-	{ 0x0, 1 },
-	{ 0x1, 2 },
-	{ 0x3, 4 },
-	{ 0x7, 8 },
-	{ 0xf, 16 },
-	{ }
-};
-
-static const struct clk_div_table clk_alpha_odd_div_table[] = {
-	{ 0x0, 1 },
-	{ 0x3, 3 },
-	{ 0x5, 5 },
-	{ 0x7, 7 },
-	{ }
-};
-
-static const struct clk_div_table clk_alpha_2bit_div_table[] = {
-	{ 0x0, 1 },
-	{ 0x1, 2 },
-	{ 0x3, 4 },
-	{ }
-};
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 			     const struct alpha_pll_config *config);
