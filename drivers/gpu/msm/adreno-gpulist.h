97d82366d05ef9c38313cd223d3b21c8b5ab56d2
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-05-09 18:58:40 +0200

    gpu: KGSL: Use ADRENO_A5XX_GPMU implementation on A530/A540
    The GPMU on Adreno A530, A540 (and v2 variants) is a simple one
    that's doing basically "everything by itself", without really
    requiring constant communication with the KGSL driver: enable
    using this implementation on these GPUs, found on legacy SoCs,
    like MSM8996 and MSM8998.
    
    Change-Id: I159314d821b3f9eda582fd28749cd622b85a50e4

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -187,7 +187,8 @@ static const struct adreno_a5xx_core adreno_gpu_core_a530v2 = {
 		DEFINE_ADRENO_REV(ADRENO_REV_A530, 5, 3, 0, 1),
 		.features = ADRENO_SPTP_PC | ADRENO_LM |
 			ADRENO_PREEMPTION | ADRENO_64BIT |
-			ADRENO_CONTENT_PROTECTION,
+			ADRENO_CONTENT_PROTECTION |
+			ADRENO_A5XX_GPMU,
 		.gpudev = &adreno_a5xx_gpudev,
 		.gmem_size = SZ_1M,
 		.busy_mask = 0xfffffffe,
@@ -211,7 +212,8 @@ static const struct adreno_a5xx_core adreno_gpu_core_a530v3 = {
 		DEFINE_ADRENO_REV(ADRENO_REV_A530, 5, 3, 0, ANY_ID),
 		.features = ADRENO_SPTP_PC | ADRENO_LM |
 			ADRENO_PREEMPTION | ADRENO_64BIT |
-			ADRENO_CONTENT_PROTECTION,
+			ADRENO_CONTENT_PROTECTION |
+			ADRENO_A5XX_GPMU,
 		.gpudev = &adreno_a5xx_gpudev,
 		.gmem_size = SZ_1M,
 		.busy_mask = 0xfffffffe,
@@ -512,7 +514,8 @@ static const struct adreno_a5xx_core adreno_gpu_core_a540v2 = {
 		DEFINE_ADRENO_REV(ADRENO_REV_A540, 5, 4, 0, ANY_ID),
 		.features = ADRENO_PREEMPTION | ADRENO_64BIT |
 			ADRENO_CONTENT_PROTECTION |
-			ADRENO_SPTP_PC,
+			ADRENO_SPTP_PC |
+			ADRENO_A5XX_GPMU,
 		.gpudev = &adreno_a5xx_gpudev,
 		.gmem_size = SZ_1M,
 		.busy_mask = 0xfffffffe,
