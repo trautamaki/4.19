91eaa67558fe331b1eaf7e417f49df3d77a8baed
Timi Rautamäki <timi.rautamaki@gmail.com>
2023-06-19 16:49:27 +0000

    clk: qcom: mdss: Introduce the 10nm driver
    Imported from 4.14
    
    Change-Id: I9fc2b5a4ab92c2dde29b75f628f9b3cfadb01ee6

--- drivers/clk/qcom/mdss/mdss-pll.c
+++ drivers/clk/qcom/mdss/mdss-pll.c
@@ -116,6 +116,9 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		goto err;
 	}
 
+	if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_10nm")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_10NM;
+	}
 	if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_14nm"))
 		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
 	else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_14nm"))
@@ -132,6 +135,8 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_DSI_PLL_12NM;
 	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_28lpm"))
 		pll_res->pll_interface_type = MDSS_DSI_PLL_28LPM;
+	else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_10nm"))
+		pll_res->pll_interface_type = MDSS_DP_PLL_10NM;
 	else
 		goto err;
 
@@ -165,6 +170,12 @@ static int mdss_pll_clock_register(struct platform_device *pdev,
 		break;
 	case MDSS_DSI_PLL_12NM:
 		rc = dsi_pll_clock_register_12nm(pdev, pll_res);
+		break;
+	case MDSS_DSI_PLL_10NM:
+		rc = dsi_pll_clock_register_10nm(pdev, pll_res);
+		break;
+	case MDSS_DP_PLL_10NM:
+		rc = dp_pll_clock_register_10nm(pdev, pll_res);
 		break;
 	case MDSS_UNKNOWN_PLL:
 	default:
@@ -398,6 +409,8 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dp_pll_sdm660"},
 	{.compatible = "qcom,mdss_dsi_pll_12nm"},
 	{.compatible = "qcom,mdss_dsi_pll_28lpm"},
+	{.compatible = "qcom,mdss_dsi_pll_10nm"},
+	{.compatible = "qcom,mdss_dp_pll_10nm"},
 	{}
 };
 
