kholk/kernel

fbdev: msm: mdss: Implement link clocks reconfiguration hack
This hack sets byte and pixel clocks to half rate and back
to normal rate when property qcom,link-clks-reconf-platform-hack
is found in the mdss-dsi-ctrl node.
It is required (temporarily) for MSM8998 on the common clock
framework due to an unknown bug appearing on suspend/resume
producing inability to resume the display.

drivers/video/fbdev/msm/mdss_dsi_clk.c
@@ -201,6 +201,20 @@ static int dsi_link_clk_set_rate(struct dsi_link_clks *l_clks)
 	if (ctrl->panel_data.panel_info.cont_splash_enabled)
 		return 0;
 
+	/*
+	 * MSM8998 on common clk framework requires the mmcc clocks
+	 * to get fully reconfigured after suspend due to an unknown
+	 * bug.
+	 * This hack will be fully removed as soon as the bug on this
+	 * SoC will get solved.
+	 */
+	if (ctrl->platform_clk_reconf_hack) {
+		rc = clk_set_rate(l_clks->clks.byte_clk,
+					l_clks->byte_clk_rate / 2);
+		rc = clk_set_rate(l_clks->clks.pixel_clk,
+					l_clks->pix_clk_rate / 2);
+	}
+
 	rc = clk_set_rate(l_clks->clks.esc_clk, l_clks->esc_clk_rate);
 	if (rc) {
 		pr_err("clk_set_rate failed for esc_clk rc = %d\n", rc);

trautamaki/4.19

drivers/video/fbdev/msm/mdss_dsi_clk.c
@@ -172,6 +172,20 @@ static int dsi_link_hs_clk_set_rate(
 	if (ctrl->panel_data.panel_info.cont_splash_enabled)
 		return 0;
 
+	/*
+	 * MSM8998 on common clk framework requires the mmcc clocks
+	 * to get fully reconfigured after suspend due to an unknown
+	 * bug.
+	 * This hack will be fully removed as soon as the bug on this
+	 * SoC will get solved.
+	 */
+	if (ctrl->platform_clk_reconf_hack) {
+		rc = clk_set_rate(link_hs_clks->byte_clk,
+					link_hs_clks->byte_clk_rate / 2);
+		rc = clk_set_rate(link_hs_clks->pixel_clk,
+					link_hs_clks->pix_clk_rate / 2);
+	}
+
 	rc = clk_set_rate(link_hs_clks->byte_clk, link_hs_clks->byte_clk_rate);
 	if (rc) {
 		pr_err("clk_set_rate failed for byte_clk rc = %d\n", rc);